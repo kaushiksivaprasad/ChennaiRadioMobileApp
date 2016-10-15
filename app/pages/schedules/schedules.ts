d93d36a909245a735025ccb2d6ade50674de1f91
vigneshwaran veeran <vignesh.intell@gmail.com>
2016-10-15 00:35:00 -0400

    timezone and schedule

--- app/pages/schedules/schedules.ts
+++ app/pages/schedules/schedules.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 import Utils from '../../utils/utils';
 import {ScheduleService} from '../../service/schedule';
 import {DomSanitizationService} from '@angular/platform-browser';
-
+import * as moment from 'moment';
+import * as tz from 'moment-timezone';
 @Component({
     templateUrl: 'build/pages/schedules/schedules.html'
 })
@@ -10,21 +11,33 @@ export class Schedules {
     private programs: Array<any> = [];
     private imgUrl: Array<any> = ["img/schedule1-min.jpg", "img/schedule2-min.jpg", "img/schedule3-min.jpg", "img/schedule4-min.jpg"];
 
-    constructor(private scheduleService: ScheduleService,private sanitizer: DomSanitizationService) {
+    constructor(private scheduleService: ScheduleService, private sanitizer: DomSanitizationService) {
+        if (this.scheduleService.schedules != null) {
+            this.setSchedule(this.scheduleService.schedules);
+        }
         this.scheduleService.scheduleRecieveEvent.subscribe(schedules => {
-            let imgcount = 0;
-            for (var sh = 0; sh = schedules.length; sh++) {
-                this.programs[sh].name = schedules[sh].programName;
-                this.programs[sh].hostedBy = schedules[sh].hostedBy;
-                this.programs[sh].time = schedules[sh].time;                
+            this.setSchedule(schedules);
+        });
+    }
+
+    setSchedule(schedules) {
+        let imgcount = 0;
+        let programCount = 0;
+        let timezoneOffset=moment.tz.guess();
+        for (var sh = 0; sh < schedules.length; sh++) {
+            for (var sp = 0; sp < schedules[sh].programs.length; sp++) {
+                this.programs[programCount]={};
+                this.programs[programCount].name = schedules[sh].programs[sp].programName;
+                this.programs[programCount].hostedBy = schedules[sh].programs[sp].hostedBy;
+                this.programs[programCount].time = schedules[sh].programs[sp].startTimeInHour.moment.tz(timezoneOffset).format('H')+":"+schedules[sh].programs[sp].startTimeInMinutes.moment.tz(timezoneOffset).format('mm');
+                
                 if (imgcount === this.imgUrl.length) {
                     imgcount = 0;
                 }
-                this.programs[sh].programImg = this.sanitizer.bypassSecurityTrustUrl(this.imgUrl[imgcount]);
+                this.programs[programCount].programImg = this.sanitizer.bypassSecurityTrustUrl(this.imgUrl[imgcount]);
                 imgcount++;
+                programCount++;
             }
-
-        });
-
+        }
     }
 }
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="globals/es6-shim/index.d.ts" />
+/// <reference path="globals/moment-timezone/index.d.ts" />
 /// <reference path="globals/moment/index.d.ts" />
